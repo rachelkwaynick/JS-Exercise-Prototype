c5afcc139f6899d6d2502343fce0e1f6822b0107
Rachel Waynick <rachelkwaynick@gmail.com>
2020-09-16 20:03:20 -0700

    MVP done

--- index.js
+++ index.js
@@ -96,13 +96,17 @@ Car.prototype.drive = function (distance) {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby(attributes, favoriteToy) {
-  Person.call(this, attributes);
+function Baby(name, age, favoriteToy) {
+  this.name = name;
+  this.age = age;
   this.favoriteToy = favoriteToy;
 }
 
 Baby.prototype = Object.create(Person.prototype)
 
+Baby.prototype.play = function () {
+  return `Playing with ${this.favoriteToy}`
+}
 
 /* 
   TASK 4
